a62ad69365ee35a9410a48a00df6cf94dd4df120
Ankit Sah <ankitkumarsah3727@gmail.com>
2021-05-03 12:38:56 +0530

    ghjk
    hjnmk

--- postblog/resources/views/show.blade.php
+++ postblog/resources/views/show.blade.php
@@ -3,31 +3,10 @@
 @section('content')
 
 @foreach($user as $row)
-<!-- <div class="container">
-    <div class="well">
-        <div class="media">
-            <a class="pull-left" href="#">
-                <h4 class="media-heading">Title</h4>
-                <p>{{$row->title}}</p>
-                <img class="media-object" width="100px" height="100px" src="{{ asset('images/' .$row->image) }}">
-            </a>
-            <div class="media-body">
-                <h4 class="media-heading">Body</h4>
-                <p>{{$row->body}}</p>
-                <p class="text-right">Description<br/>{{$row->dscription}}</p>
-               
-                <ul class="list-inline list-unstyled">
-                    <li><span><i class="glyphicon glyphicon-edit"></i> Edit</span></li>
-                    <li>|</li>
-                    <span><i class="glyphicon glyphicon-trash"></i> Delete</span>
 
-                </ul>
-            </div>
-        </div>
-    </div>
-</div> -->
 <div class="container">
-    <div class="row" style="border-left:5px solid red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius:10px">
+    <div class="row"
+        style="border-left:5px solid red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius:10px">
         <div class="col-sm-2" style="padding:15px 15px 15px 15px">
             <h4 class="media-heading">Title</h4>
             <p>{{$row->title}}</p>
@@ -39,7 +18,8 @@
         </div>
         <div class="col-sm-2" style="padding:15px 15px 15px 15px">
 
-            <p class="text-right"><h4 class="media-heading">Description</h4>{{$row->dscription}}</p>
+            <p class="text-right">
+            <h4 class="media-heading">Description</h4>{{$row->dscription}}</p>
         </div>
         <div class="col-sm-2" style="padding:50px 15px 15px 15px">
 
